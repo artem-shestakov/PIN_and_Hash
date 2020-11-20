8c849aa2f0e28f1fc415ac40b15bc16c72ba31b4
Artem Shestakov <artem.s.shestakov@yandex.ru>
2020-11-20 12:53:32 +0300

    Add 2 letter in pin

--- app/api/v1/routes.py
+++ app/api/v1/routes.py
@@ -12,10 +12,19 @@ api_v1_bp = Blueprint(
 def get_pin():
     pin_len = request.args.get('pin_len')
     salt_len = request.args.get('salt_len')
+    strong = request.args.get('strong')
+    print(type(strong))
+    if isinstance(strong, str):
+        strong = True
+        print('true')
+    else:
+        strong = False
+        print('false')
     if not salt_len:
         salt_len = current_app.config['SALT_LEN']
     if not pin_len:
         pin_len = current_app.config['PIN_LEN']
-    resp = hash_generator(int(pin_len), int(salt_len))
+
+    resp = hash_generator(int(pin_len), int(salt_len), strong)
     current_app.logger.info(f"{request.method} {request.base_url} Request PIN and Hash from {request.remote_addr}")
     return resp
